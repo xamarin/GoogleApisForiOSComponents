28982e15e408816c20afb988dad9805be7dec65a
SotoiGhost <israel.soto@xamarin.com>
2016-06-16 14:35:46 -0500

    [analytics] Removed Field Exporter project, added missing Constants

--- Google.Analytics/source/Google.Analytics/libGAIFieldExporter.linkwith.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("libGAIFieldExporter.a", 
-	LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.Simulator64 | LinkTarget.Arm64,
-	ForceLoad = true,
-	SmartLink = true)]
