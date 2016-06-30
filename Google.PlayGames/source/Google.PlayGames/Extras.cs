01fd571fd0d9d86828392ddbb55f47bcd94b6e68
SotoiGhost <issoto@microsoft.com>
2016-06-29 19:41:18 -0500

    [playgames] Removed Field exporter integration

--- Google.PlayGames/source/Google.PlayGames/Extras.cs
+++ Google.PlayGames/source/Google.PlayGames/Extras.cs
@@ -3,31 +3,31 @@ using Foundation;
 
 namespace Google.Play.GameServices
 {
-    public partial class RealTimeRoom
-    {
+	public partial class RealTimeRoom
+	{
 
-        public int SendReliableData (NSData data, RealTimeParticipant [] participants)
-        {
-            var array = NSArray.FromObjects (participants);
-            return _SendReliableData (data, array);
-        }
+		public int SendReliableData (NSData data, RealTimeParticipant [] participants)
+		{
+			var array = NSArray.FromObjects (participants);
+			return _SendReliableData (data, array);
+		}
 
-        public int SendReliableData (NSData data, string [] participantsId)
-        {
-            var array = NSArray.FromObjects (participantsId);
-            return _SendReliableData (data, array);
-        }
+		public int SendReliableData (NSData data, string [] participantsId)
+		{
+			var array = NSArray.FromObjects (participantsId);
+			return _SendReliableData (data, array);
+		}
 
-        public void SendUnreliableData (NSData data, RealTimeParticipant [] participants)
-        {
-            var array = NSArray.FromObjects (participants);
-            _SendUnreliableData (data, array);
-        }
+		public void SendUnreliableData (NSData data, RealTimeParticipant [] participants)
+		{
+			var array = NSArray.FromObjects (participants);
+			_SendUnreliableData (data, array);
+		}
 
-        public void SendUnreliableData (NSData data, string [] participantsId)
-        {
-            var array = NSArray.FromObjects (participantsId);
-            _SendUnreliableData (data, array);
-        }
-    }
+		public void SendUnreliableData (NSData data, string [] participantsId)
+		{
+			var array = NSArray.FromObjects (participantsId);
+			_SendUnreliableData (data, array);
+		}
+	}
 }
