bb756bf1aca9533c11573938133f433ff098e3e2
SotoiGhost <issoto@microsoft.com>
2016-07-22 01:50:27 -0500

    [google][signin] Updated to version 4.0.0 - Updated to be used with Firebase.Invites - Prepared to work with G.Core and F.Core dependencies

--- Google.SignIn/source/Google.SignIn/libGIPNSURL+FIFE_external.linkwith.cs
+++
@@ -1,5 +0,0 @@
-using ObjCRuntime;
-
-[assembly: LinkWith ("libGIPNSURL+FIFE_external.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
-	SmartLink = true,
-	ForceLoad = true)]
