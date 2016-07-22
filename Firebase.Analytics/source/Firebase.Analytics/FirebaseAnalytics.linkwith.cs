8bb9d9089d596863c95dff35f45e33f03f9b7849
SotoiGhost <issoto@microsoft.com>
2016-07-22 01:43:25 -0500

    [google][firebase][core] Fixed some stuff in G.Core, F.Analytics and F.AdMob - Globalizing syntax of .targets files - Added a missing framework in F.Analytics - Added missing G.Analytics and G.SignIn extension frameworks to G.Core

--- Firebase.Analytics/source/Firebase.Analytics/FirebaseAnalytics.linkwith.cs
+++ Firebase.Analytics/source/Firebase.Analytics/FirebaseAnalytics.linkwith.cs
@@ -3,7 +3,7 @@ using ObjCRuntime;
 
 [assembly: LinkWith ("FirebaseAnalytics", 
                      LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
-                     Frameworks = "AddressBook AdSupport SafariServices StoreKit SystemConfiguration",
+                     Frameworks = "AddressBook AdSupport CoreGraphics SafariServices StoreKit SystemConfiguration",
                      LinkerFlags = "-lc++ -lsqlite3 -lz",
                      ForceLoad = true,
                      SmartLink = true)]
--- Google.Core/source/Google.Core/Loader.cs
+++ Google.Core/source/Google.Core/Loader.cs
@@ -24,6 +24,3 @@ namespace ApiDefinition
 		}
 	}
 }
-
-
-
