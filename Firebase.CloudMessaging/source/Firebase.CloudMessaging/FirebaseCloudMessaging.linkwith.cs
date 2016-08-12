1bc662f35ce1bd03dffdaa2347b5d6c991caa972
SotoiGhost <issoto@microsoft.com>
2016-08-11 19:42:59 -0500

    [firebase][cloudmessaging] Added Cloud Messaging component stuff - Added binding version 1.1.0 - Added externals files - Added component.yaml - Added nuget file - Added .targets file

---
+++ Firebase.CloudMessaging/source/Firebase.CloudMessaging/FirebaseCloudMessaging.linkwith.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using ObjCRuntime;
+
+[assembly: LinkWith ("FirebaseMessaging",
+	LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+        Frameworks = "AddressBook SystemConfiguration",
+        LinkerFlags = "-lsqilte3",
+        SmartLink = true,
+	ForceLoad = true)]
