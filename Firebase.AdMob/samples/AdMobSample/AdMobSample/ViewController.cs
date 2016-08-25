3764ac635e9a23bc0da2e131edf593d03fda8e96
SotoiGhost <issoto@microsoft.com>
2016-08-25 14:26:36 -0500

    [firebase][admob] Added a quick sample to verify the binding

---
+++ Firebase.AdMob/samples/AdMobSample/AdMobSample/ViewController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using UIKit;
+
+using Firebase.InstanceID;
+using Firebase.Analytics;
+using Google.MobileAds;
+
+namespace AdMobSample
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController (IntPtr handle) : base (handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			var type1 = typeof (InstanceId);
+			var type2 = typeof (Analytics);
+			var type3 = typeof (MobileAds);
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
