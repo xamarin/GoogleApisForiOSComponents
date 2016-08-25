16a0d2a4bc0464c328905f4b647f15a418df7386
SotoiGhost <issoto@microsoft.com>
2016-08-25 14:29:45 -0500

    [google][signin] Updated reference on sample Known issues: - This sample does not work with "Incremental builds" option

--- Google.SignIn/source/Google.SignIn/GoogleSignIn.linkwith.cs
+++ Google.SignIn/source/Google.SignIn/GoogleSignIn.linkwith.cs
@@ -3,7 +3,7 @@ using ObjCRuntime;
 [assembly: LinkWith ("GoogleSignIn",
 	LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
 	LinkerFlags = "-ObjC -lsqlite3",
-	Frameworks = "CoreText Security StoreKit",
+	Frameworks = "CoreText Security",
 	WeakFrameworks = "SafariServices",
 	SmartLink = true,
 	ForceLoad = true)]
