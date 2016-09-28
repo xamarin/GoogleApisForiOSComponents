ea359a048fbdc31c9f8e0a4f7143f21b752599eb
SotoiGhost <issoto@microsoft.com>
2016-09-28 10:11:32 -0500

    [firebase][dynamiclinks] Updated to version 1.3.0

--- Firebase.DynamicLinks/source/Firebase.DynamicLinks/FirebaseDynamicLinks.linkwith.cs
+++ Firebase.DynamicLinks/source/Firebase.DynamicLinks/FirebaseDynamicLinks.linkwith.cs
@@ -3,6 +3,7 @@ using ObjCRuntime;
 
 [assembly: LinkWith ("FirebaseDynamicLinks",
 		     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
-		     Frameworks = "AssetsLibrary CoreLocation CoreMotion MessageUI QuartzCore SafariServices",
+		     Frameworks = "AssetsLibrary CoreMotion MessageUI QuartzCore",
+                     WeakFrameworks = "WebKit",
 		     ForceLoad = true,
 		     SmartLink = true)]
