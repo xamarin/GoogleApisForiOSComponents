9a907b40360cedb5cb9182597189b728fd2b1b0a
SotoiGhost <issoto@microsoft.com>
2016-10-05 14:13:28 -0500

    [firebase][auth] Added binding

---
+++ Firebase.Auth/source/Firebase.Auth/Structs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace Firebase.Auth
+{
+	[Native]
+	public enum AuthErrorCode : long
+	{
+		InvalidCustomToken = 17000,
+		CustomTokenMismatch = 17002,
+		InvalidCredential = 17004,
+		UserDisabled = 17005,
+		OperationNotAllowed = 17006,
+		EmailAlreadyInUse = 17007,
+		InvalidEmail = 17008,
+		WrongPassword = 17009,
+		TooManyRequests = 17010,
+		UserNotFound = 17011,
+		AccountExistsWithDifferentCredential = 17012,
+		RequiresRecentLogin = 17014,
+		ProviderAlreadyLinked = 17015,
+		NoSuchProvider = 17016,
+		InvalidUserToken = 17017,
+		NetworkError = 17020,
+		UserTokenExpired = 17021,
+		InvalidAPIKey = 17023,
+		UserMismatch = 17024,
+		CredentialAlreadyInUse = 17025,
+		WeakPassword = 17026,
+		AppNotAuthorized = 17028,
+		KeychainError = 17995,
+		InternalError = 17999
+	}
+}
