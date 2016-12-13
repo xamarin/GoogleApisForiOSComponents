afd1bc9f397faaa22c722b0fbb6b8ce5fff9cc78
SotoiGhost <issoto@microsoft.com>
2016-12-13 15:36:06 -0600

    [google][signin] Updated to version 4.0.1

--- Google.SignIn/source/Google.SignIn/GoogleSignIn.linkwith.cs
+++ Google.SignIn/source/Google.SignIn/GoogleSignIn.linkwith.cs
@@ -1,24 +1,20 @@
 using ObjCRuntime;
 
 [assembly: LinkWith ("GoogleSignIn",
-	LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
-	LinkerFlags = "-ObjC -lsqlite3",
-	Frameworks = "CoreText Security",
-	WeakFrameworks = "SafariServices",
-	SmartLink = true,
-	ForceLoad = true)]
+                     LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
+                     LinkerFlags = "-ObjC -lsqlite3",
+                     Frameworks = "CoreText Security",
+                     WeakFrameworks = "SafariServices",
+                     SmartLink = true,
+                     ForceLoad = true)]
 
-[assembly: LinkWith ("GoogleNetworkingUtilities",
-	LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
-	SmartLink = true,
-	ForceLoad = true)]
+[assembly: LinkWith ("GTMOAuth2",
+                     LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
+                     Frameworks = "Security SystemConfiguration",
+                     SmartLink = true,
+                     ForceLoad = true)]
 
 [assembly: LinkWith ("GoogleAppUtilities",
-	LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
-	SmartLink = true,
-	ForceLoad = true)]
-
-[assembly: LinkWith ("GoogleAuthUtilities",
-	LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
-	SmartLink = true,
-	ForceLoad = true)]
+                     LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64,
+                     SmartLink = true,
+                     ForceLoad = true)]
