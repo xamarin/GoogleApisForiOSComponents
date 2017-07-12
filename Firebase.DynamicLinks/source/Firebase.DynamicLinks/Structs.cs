b5c57724e03661c8b7b04e8500ad110eaa525c79
SotoiGhost <issoto@microsoft.com>
2017-07-11 21:50:56 -0500

    [firebase][dynamiclinks] Updated to version 2.0.0.0

--- Firebase.DynamicLinks/source/Firebase.DynamicLinks/Structs.cs
+++ Firebase.DynamicLinks/source/Firebase.DynamicLinks/Structs.cs
@@ -4,6 +4,14 @@ using ObjCRuntime;
 namespace Firebase.DynamicLinks
 {
 	[Native]
+	public enum ShortDynamicLinkPathLength : long
+	{
+		Default = 0,
+		Short,
+		Unguessable
+	}
+
+	[Native]
 	public enum DynamicLinkMatchConfidence : ulong
 	{
 		Weak,
