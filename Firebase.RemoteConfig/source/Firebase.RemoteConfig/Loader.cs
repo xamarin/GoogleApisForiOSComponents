9840fbb0c9cc070162899a776e00a26de70e0717
SotoiGhost <issoto@microsoft.com>
2017-07-17 19:13:18 -0500

    [firebase][remoteconfig] Updated to version 2.0.1.0

---
+++ Firebase.RemoteConfig/source/Firebase.RemoteConfig/Loader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Firebase.RemoteConfig
+{
+	public class Loader
+	{
+		static Loader ()
+		{
+			Firebase.InstanceID.Loader.ForceLoad ();
+			Firebase.Core.Loader.ForceLoad ();
+			Firebase.Analytics.Loader.ForceLoad ();
+		}
+
+		public static void ForceLoad () { }
+	}
+}
+
+namespace ApiDefinition
+{
+	partial class Messaging
+	{
+		static Messaging ()
+		{
+			Firebase.RemoteConfig.Loader.ForceLoad ();
+		}
+	}
+}
