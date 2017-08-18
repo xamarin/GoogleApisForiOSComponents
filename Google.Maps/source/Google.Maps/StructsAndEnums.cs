24c4209271f52f72a4bbde7abb6490f91de67383
campersau <buchholz.bastian@googlemail.com>
2017-08-18 08:43:24 +0200

    Update Google.Maps to 2.4

--- Google.Maps/source/Google.Maps/StructsAndEnums.cs
+++ Google.Maps/source/Google.Maps/StructsAndEnums.cs
@@ -31,17 +31,6 @@ namespace Google.Maps
 		Maximum,
 	}
 
-	[Native]
-	public enum PlacesAutocompleteTypeFilter : long
-	{
-		NoFilter,
-		Geocode,
-		Address,
-		Establishment,
-		Region,
-		City
-	}
-
 	public enum MarkerAnimation
 	{
 		None = 0,
@@ -60,50 +49,6 @@ namespace Google.Maps
 		Geodesic,
 		Rhumb,
 		Projected
-	}
-
-	[Native]
-	public enum PlacesOpenNowStatus : long
-	{
-		Yes,
-		No,
-		Unknown
-	}
-
-	[Native]
-	public enum PlacesPriceLevel : long
-	{
-		Unknown = -1,
-		Free = 0,
-		Cheap = 1,
-		Medium = 2,
-		High = 3,
-		Expensive = 4
-	}
-
-	[Native]
-	public enum PlacePickerErrorCode : long
-	{
-		UnknownError = -1,
-		InternalError = -2,
-		InvalidConfig = -3,
-		OverlappingCalls = -4
-	}
-
-	[Native]
-	public enum PlacesErrorCode : long
-	{
-		NetworkError = -1,
-		ServerError = -2,
-		InternalError = -3,
-		KeyInvalid = -4,
-		KeyExpired = -5,
-		UsageLimitExceeded = -6,
-		RateLimitExceeded = -7,
-		DeviceRateLimitExceeded = -8,
-		AccessNotConfigured = -9,
-		IncorrectBundleIdentifier = -10,
-		LocationError = -11
 	}
 
 	[StructLayout (LayoutKind.Sequential)]
