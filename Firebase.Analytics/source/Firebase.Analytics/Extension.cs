fc2272abb68b5ad6df9793b9e50bc65e1d8522a7
SotoiGhost <issoto@microsoft.com>
2017-09-13 22:07:15 -0500

    [firebase][analytics] Forgot to remove some commented code

--- Firebase.Analytics/source/Firebase.Analytics/Extension.cs
+++ Firebase.Analytics/source/Firebase.Analytics/Extension.cs
@@ -3,15 +3,6 @@ using Foundation;
 
 namespace Firebase.Analytics
 {
-	//public partial class App
-	//{
-	//	[Obsolete ("Use From method instead.")]
-	//	public static App Get (string name)
-	//	{
-	//		return From (name);
-	//	}
-	//}
-
 	public partial class Analytics
 	{
 		public static void LogEvent (NSString name, NSDictionary<NSString, NSObject> parameters)
