ed5efbd475430a62d13daf0c5e3813bf10de5084
SotoiGhost <issoto@microsoft.com>
2017-09-13 21:53:37 -0500

    [google][mobileads] Removed Armv7s arch

--- Google.MobileAds/source/Google.MobileAds/GoogleMobileAds.linkwith.cs
+++ Google.MobileAds/source/Google.MobileAds/GoogleMobileAds.linkwith.cs
@@ -2,7 +2,7 @@ using System;
 using ObjCRuntime;
 
 [assembly: LinkWith ("GoogleMobileAds",
-	LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+	LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
 	LinkerFlags = "-ObjC",
 	Frameworks = "AudioToolbox AVFoundation CoreGraphics CoreMedia CoreMotion CoreTelephony CoreVideo GLKit MediaPlayer MessageUI MobileCoreServices OpenGLES StoreKit SystemConfiguration",
         WeakFrameworks = "AdSupport JavaScriptCore SafariServices WebKit",
