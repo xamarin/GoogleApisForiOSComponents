de3e5f985f5d2e1f9fc32d0a0d4445ea2bef2651
SotoiGhost <issoto@microsoft.com>
2017-10-27 12:33:59 -0500

    [firebase][performancemonitoring] Added binding

---
+++ Firebase.PerformanceMonitoring/source/Firebase.PerformanceMonitoring/ApiDefinition.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using ObjCRuntime;
+using CoreGraphics;
+
+namespace Firebase.PerformanceMonitoring
+{
+	// @interface FIRTrace : NSObject
+	[DisableDefaultCtor]
+	[BaseType (typeof (NSObject), Name = "FIRTrace")]
+	interface Trace
+	{
+		// @property (readonly, copy, nonatomic) NSString * _Nonnull name;
+		[Export ("name")]
+		string Name { get; }
+
+		// -(void)start;
+		[Export ("start")]
+		void Start ();
+
+		// -(void)stop;
+		[Export ("stop")]
+		void Stop ();
+
+		// -(void)incrementCounterNamed:(NSString * _Nonnull)counterName;
+		[Export ("incrementCounterNamed:")]
+		void IncrementCounter (string counterName);
+
+		// -(void)incrementCounterNamed:(NSString * _Nonnull)counterName by:(NSInteger)incrementValue;
+		[Export ("incrementCounterNamed:by:")]
+		void IncrementCounter (string counterName, nint incrementValue);
+	}
+
+	// @interface FIRPerformance : NSObject
+	[BaseType (typeof (NSObject), Name = "FIRPerformance")]
+	interface Performance
+	{
+		// @property (getter = isDataCollectionEnabled, assign, nonatomic) BOOL dataCollectionEnabled;
+		[Export ("dataCollectionEnabled")]
+		bool DataCollectionEnabled { [Bind ("isDataCollectionEnabled")] get; set; }
+
+		// @property (getter = isInstrumentationEnabled, assign, nonatomic) BOOL instrumentationEnabled;
+		[Export ("instrumentationEnabled")]
+		bool InstrumentationEnabled { [Bind ("isInstrumentationEnabled")] get; set; }
+
+		// +(instancetype _Nonnull)sharedInstance;
+		[Static]
+		[Export ("sharedInstance")]
+		Performance SharedInstance { get; }
+
+		// +(FIRTrace * _Nullable)startTraceWithName:(NSString * _Nonnull)name;
+		[Static]
+		[return: NullAllowed]
+		[Export ("startTraceWithName:")]
+		Trace StartTrace (string name);
+
+		// -(FIRTrace * _Nullable)traceWithName:(NSString * _Nonnull)name;
+		[Export ("traceWithName:")]
+		[return: NullAllowed]
+		Trace GetTrace (string name);
+	}
+}
---
+++ Firebase.PerformanceMonitoring/source/Firebase.PerformanceMonitoring/FirebasePerformanceMonitoring.linkwith.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using ObjCRuntime;
+
+[assembly: LinkWith ("FirebasePerformanceMonitoring",
+		     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+                     Frameworks = "CoreTelephony SystemConfiguration",
+                     LinkerFlags = "-lc++",
+		     ForceLoad = true,
+		     SmartLink = true)]
