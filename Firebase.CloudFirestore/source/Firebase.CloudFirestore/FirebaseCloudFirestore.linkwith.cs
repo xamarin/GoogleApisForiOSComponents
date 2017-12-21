34144e06dace5bbf397c44ba314c879d21457ce9
SotoiGhost <issoto@microsoft.com>
2017-12-21 10:24:41 -0600

    [Firebase][CloudFirestore] Added targets and linkwith file

--- Firebase.CloudFirestore/source/Firebase.CloudFirestore/FirebaseCloudFirestore.linkwith.cs
+++ Firebase.CloudFirestore/source/Firebase.CloudFirestore/FirebaseCloudFirestore.linkwith.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using ObjCRuntime;
+
+[assembly: LinkWith ("FirebaseFirestore",
+		     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+                     Frameworks = "MobileCoreServices",
+		     SmartLink = true,
+		     ForceLoad = true)]
