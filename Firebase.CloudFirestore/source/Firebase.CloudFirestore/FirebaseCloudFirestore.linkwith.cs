c99f050458f5471466d91131fa9449898a4f9ecb
SotoiGhost <issoto@microsoft.com>
2017-12-21 11:11:37 -0600

    [Firebase][CloudFirestore] Added dependency libraries to binding project

--- Firebase.CloudFirestore/source/Firebase.CloudFirestore/FirebaseCloudFirestore.linkwith.cs
+++ Firebase.CloudFirestore/source/Firebase.CloudFirestore/FirebaseCloudFirestore.linkwith.cs
@@ -5,3 +5,28 @@
                      Frameworks = "MobileCoreServices",
 		     SmartLink = true,
 		     ForceLoad = true)]
+
+[assembly: LinkWith ("BoringSSL",
+		     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+		     SmartLink = true,
+		     ForceLoad = true)]
+
+[assembly: LinkWith ("gRPC",
+		     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+		     SmartLink = true,
+		     ForceLoad = true)]
+
+[assembly: LinkWith ("gRPC-Core",
+		     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+		     SmartLink = true,
+		     ForceLoad = true)]
+
+[assembly: LinkWith ("gRPC-ProtoRPC",
+		     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+		     SmartLink = true,
+		     ForceLoad = true)]
+
+[assembly: LinkWith ("gRPC-RxLibrary",
+		     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+		     SmartLink = true,
+		     ForceLoad = true)]
