d4c489f29d51f11972018e28f3a387aaa30ef46c
SotoiGhost <issoto@microsoft.com>
2018-04-07 14:10:47 -0500

    [Firebase][Crashlytics] Added sample

---
+++ Firebase.Crashlytics/samples/CrashlyticsSample/CrashlyticsSample/ViewController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using UIKit;
+using CoreGraphics;
+using Firebase.Crashlytics;
+using ObjCRuntime;
+
+namespace CrashlyticsSample {
+	public partial class ViewController : UIViewController {
+		protected ViewController (IntPtr handle) : base (handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			var button = new UIButton (UIButtonType.RoundedRect) {
+				Frame = new CGRect (0, 0, 100, 30),
+				TranslatesAutoresizingMaskIntoConstraints = false
+			};
+			button.SetTitle ("Crash", UIControlState.Normal);
+			button.TouchUpInside += Button_TouchUpInside;
+			View.AddSubview (button);
+
+			View.AddConstraint (NSLayoutConstraint.Create (button, NSLayoutAttribute.CenterX, NSLayoutRelation.Equal, 
+			                                               View, NSLayoutAttribute.CenterX, 
+			                                               1, 0));
+			View.AddConstraint (NSLayoutConstraint.Create (button, NSLayoutAttribute.CenterY, NSLayoutRelation.Equal,
+			                                               View, NSLayoutAttribute.CenterY,
+			                                               1, 0));
+		}
+
+		void Button_TouchUpInside (object sender, EventArgs e) => Crashlytics.SharedInstance.Crash ();
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
