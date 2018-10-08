6fedf74c249704f2cfb01cb66f46c643ecbba89e
SotoiGhost <issoto@microsoft.com>
2018-10-08 14:20:17 -0500

    [Task699754] Added the Firebase ML Model Interpreter binding

--- Firebase.MLKit/source/Firebase.MLKit/Structs.cs
+++ Firebase.MLKit/source/Firebase.MLKit/Structs.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using ObjCRuntime;
 
-namespace Firebase.MLKit {
-
+namespace Firebase.MLKit.ModelInterpreter {
+	[Native]
+	public enum ModelElementType : ulong {
+		Unknown = 0,
+		Float32,
+		Int32,
+		UInt8,
+		Int64
+	}
 }
