95ccdc5c1a12d97596c679538449dcfb6c3aca57
SotoiGhost <issoto@microsoft.com>
2019-01-14 20:02:17 -0600

    [Google][SignIn] Updated to version 4.4.0.0

--- Google.SignIn/source/Google.SignIn/Properties/AssemblyInfo.cs
+++ Google.SignIn/source/Google.SignIn/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using Foundation;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Microsoft")]
+[assembly: AssemblyCopyright ("© Microsoft Corporation. All rights reserved.")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -26,7 +26,7 @@ using Foundation;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("4.2.0.0")]
+[assembly: AssemblyFileVersion ("4.4.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
