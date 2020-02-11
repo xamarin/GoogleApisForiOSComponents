e69b31d5761353411d8dee36064d608032ddb46f
SotoiGhost <issoto@microsoft.com>
2020-02-11 16:57:40 -0600

    Replaced RestoreAssemblyResource with BundleResource and NativeReference

--- source/Firebase/CrashReporting/FirebaseCrashReporting.linkwith.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("FirebaseCrash",
-                     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
-                     Frameworks = "CoreTelephony SystemConfiguration",
-                     LinkerFlags = "-lc++",
-                     SmartLink = true,
-                     ForceLoad = true)]
--- source/Google/AppInvite/GPPCore.linkwith.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("GPPCore", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
-
-
