3c9ae3ed44c582679af04a8fde109f3c9e507503
Robin Fischer <t9RCtCxQZFp@gmail.com>
2020-03-28 12:36:28 +0100

    Add simple ui to call functions

--- samples/Firebase/Functions/FunctionsSample/MenuViewController.cs
+++ samples/Firebase/Functions/FunctionsSample/MenuViewController.cs
@@ -1,18 +1,39 @@
-﻿using System;
-using MonoTouch.Dialog;
+﻿using MonoTouch.Dialog;
 using UIKit;
 using Foundation;
-using ObjCRuntime;
+using Firebase.Functions;
+using System;
 
 namespace FunctionsSample
 {
 	public class MenuViewController : DialogViewController
 	{
+		EntryElement functionEntry;
+
+		EntryElement dataEntry;
+
 		public MenuViewController () : base (UITableViewStyle.Grouped, null, true)
 		{
-			Root = new RootElement ("Firebase Functions Sample") {
+            functionEntry = new EntryElement("Function", "functionName", "helloWorld");
+			dataEntry = new EntryElement("Data", "sample data string", "");
+
+            Root = new RootElement("Firebase Functions Sample") {
 				new Section ("Select your function") {
-					new StringElement ("Password Authentication", () => OpenViewController (new UserViewController ())) {
+					functionEntry,
+					dataEntry,
+					new StringElement ("Call function", () => {
+						NSObject data;
+						if (string.IsNullOrEmpty(dataEntry.Value) == false)
+						{
+							data = FromObject(dataEntry.Value);
+						}
+						else
+						{
+							data = null;
+						}
+						CallFunction(functionEntry.Value, data);
+                        })
+                    {
 						Alignment = UITextAlignment.Center
 					}
 				}
@@ -22,6 +43,42 @@ namespace FunctionsSample
 		void OpenViewController (UIViewController viewController)
 		{
 			NavigationController.PushViewController (viewController, true);
+		}
+
+		private void CallFunction(string functionName, NSObject data)
+		{
+			var instance = Functions.DefaultInstance;
+
+			var function = instance.HTTPSCallableWithName(functionName);
+
+            if (data == null)
+            {
+			    function.CallWithCompletion(OnFunctionCompletion);
+            }
+            else
+            {
+				function.CallWithObject(data, OnFunctionCompletion);
+            }
+		}
+
+        private void OnFunctionCompletion(HTTPSCallableResult result, NSError error)
+        {
+			Console.WriteLine("Got result: " + result);
+            if (error != null)
+            {
+				Console.WriteLine("Domain: " + error.Domain + " Error: " + error.DebugDescription);
+                if (error.Domain == Functions.FunctionsErrorDomain)
+                {
+					FunctionsErrorCode code = (FunctionsErrorCode)(long)error.Code;
+					Console.WriteLine("Error Code: " + code);
+				}
+				AppDelegate.ShowMessage("Error", error.DebugDescription, NavigationController);
+            }
+			else if (result != null)
+			{
+				Console.WriteLine("Data: " + result.Data);
+				AppDelegate.ShowMessage("Data", result.Data.ToString(), NavigationController);
+			}
 		}
 	}
 }
