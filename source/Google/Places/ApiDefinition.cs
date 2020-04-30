67bd471c2a35ef77306a94bb698474c8d2563933
Edward Brey <edward@breyfamily.net>
2020-04-30 05:09:33 -0500

    Fix "supermarket" missing from PlaceTypes
    Fixes #401. This is a breaking change: the type of `Google.Places.Types` is changed from `PlaceType[]` to `string[]`.

--- source/Google/Places/ApiDefinition.cs
+++ source/Google/Places/ApiDefinition.cs
@@ -20,7 +20,6 @@ namespace Google.Places
 		NSString Type { get; }
 
 		// @property (readonly, nonatomic, strong) NSArray<NSString *> * _Nonnull types;
-		[BindAs (typeof (PlaceType []))]
 		[Export ("types", ArgumentSemantic.Strong)]
 		NSString [] Types { get; }
 
