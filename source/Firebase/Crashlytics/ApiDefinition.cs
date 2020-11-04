d982cf10c40ef754bb79145a3fb1b8a340e27de0
Daniel Gatto <d.mario.gatto@gmail.com>
2020-11-05 00:07:53 +1030

    [Firebase][Crashlytics] Fix binding parameter type

--- source/Firebase/Crashlytics/ApiDefinition.cs
+++ source/Firebase/Crashlytics/ApiDefinition.cs
@@ -101,7 +101,7 @@ namespace Firebase.Crashlytics {
 		// + (instancetype)stackFrameWithAddress:(NSUInteger)address;
 		[Static]
 		[Export ("stackFrameWithAddress:address")]
-		StackFrame Create (nint address);
+		StackFrame Create (nuint address);
 
 		// +(instancetype _Nonnull)stackFrameWithSymbol:(NSString * _Nonnull)symbol file:(NSString * _Nonnull)file line:(NSInteger)line __attribute__((availability(swift, unavailable)));
 		[Static]
