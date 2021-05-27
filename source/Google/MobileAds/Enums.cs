cd37bd149e96197e57bde1c4fdeaf97ab75bff09
Daniel Gatto <d.mario.gatto@gmail.com>
2021-05-27 21:10:23 +0930

    [Google][AdMob][UMP] Updated to 8.5.0, added UMP 1.1.0

--- source/Google/MobileAds/Enums.cs
+++ source/Google/MobileAds/Enums.cs
@@ -8,11 +8,14 @@ using ObjCRuntime;
 namespace Google.MobileAds
 {
 	[Native]
-	public enum AdFormat : long {
+	public enum AdFormat : long
+	{
 		Banner,
 		Interstitial,
 		Rewarded,
-		Native
+		Native,
+		RewardedInterstitial,
+		Unknown
 	}
 
 	public enum AdLoaderAdType
@@ -108,58 +111,5 @@ namespace Google.MobileAds
 		TopLeftCorner,
 		BottomRightCorner,
 		BottomLeftCorner
-	}
-}
-
-namespace Google.MobileAds.Consent {
-	[Native]
-	public enum ConsentStatus : long
-	{
-		Unknown = 0,
-		Required = 1,
-		NotRequired = 2,
-		Obtained = 3
-	}
-
-	[Native]
-	public enum ConsentType : long
-	{
-		Unknown = 0,
-		Personalized = 1,
-		NonPersonalized = 2
-	}
-
-	[Native]
-	public enum FormStatus : long
-	{
-		Unknown = 0,
-		Available = 1,
-		Unavailable = 2
-	}
-
-	[Native]
-	public enum DebugGeography : long
-	{
-		Disabled = 0,
-		Eea = 1,
-		NotEea = 2
-	}
-
-	[Native]
-	public enum RequestErrorCode : long
-	{
-		Internal = 1,
-		InvalidAppID = 2,
-		Network = 3,
-		Misconfiguration = 4
-	}
-
-	[Native]
-	public enum FormErrorCode : long
-	{
-		Internal = 5,
-		AlreadyUsed = 6,
-		Unavailable = 7,
-		Timeout = 8
 	}
 }
