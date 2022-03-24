8ea986d497075f783a2efedd050ef7bba00a2012
Israel Soto <issoto@microsoft.com>
2022-03-24 17:08:17 -0600

    [Firebase/AdMob] Added sample to test .net6.0

---
+++ samples/Firebase/Auth/AuthSample/AppDelegate.cs
@@ -0,0 +1,44 @@
+using Xamarin.iOS.Shared.Helpers;
+
+namespace AuthSample;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+		// You can download your GoogleService-Info.plist file following the next link:
+		// https://firebase.google.com/docs/ios/setup
+		if (!GoogleServiceInfoPlistHelper.FileExist ()) {
+			Window = GoogleServiceInfoPlistHelper.CreateWindowWithFileNotFoundMessage ();
+			return true;
+		}
+
+		var coreType = typeof (Firebase.Core.App);
+		var authType = typeof (Firebase.Auth.Auth);
+		Firebase.Core.App.Configure ();
+
+		// create a UIViewController with a single UILabel
+		var vc = new UIViewController ();
+		vc.View!.AddSubview (new UILabel (Window!.Frame) {
+			BackgroundColor = UIColor.White,
+			TextAlignment = UITextAlignment.Center,
+			Lines = 0,
+			Text = $"Hello, iOS!\nRunning {nameof (AuthSample)} with .net6.0!"
+		});
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		return true;
+	}
+}
+
