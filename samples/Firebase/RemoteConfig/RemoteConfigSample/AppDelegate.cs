d6bbafcffb14c66d29aecd992586e46cd21322a2
Israel Soto <issoto@microsoft.com>
2022-03-25 18:17:06 -0600

    [Firebase/RemoteConfig] Added sample to test .net6.0

---
+++ samples/Firebase/RemoteConfig/RemoteConfigSample/AppDelegate.cs
@@ -0,0 +1,46 @@
+using Xamarin.iOS.Shared.Helpers;
+
+namespace RemoteConfigSample;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+		// You can download your GoogleService-Info.plist file following the next link:
+		// https://firebase.google.com/docs/ios/setup
+		if (!GoogleServiceInfoPlistHelper.FileExist ()) {
+			Window = GoogleServiceInfoPlistHelper.CreateWindowWithFileNotFoundMessage ();
+			return true;
+		}
+
+		var coreType = typeof (Firebase.Core.App);
+		var installationsType = typeof (Firebase.Installations.Installations);
+		var abTestingType = typeof (Firebase.ABTesting.ExperimentController);
+		var remoteConfigType = typeof (Firebase.RemoteConfig.RemoteConfig);
+		Firebase.Core.App.Configure ();
+
+		// create a UIViewController with a single UILabel
+		var vc = new UIViewController ();
+		vc.View!.AddSubview (new UILabel (Window!.Frame) {
+			BackgroundColor = UIColor.White,
+			TextAlignment = UITextAlignment.Center,
+			Lines = 0,
+			Text = $"Hello, iOS!\nRunning {nameof (RemoteConfigSample)} with .net6.0!"
+		});
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		return true;
+	}
+}
+
