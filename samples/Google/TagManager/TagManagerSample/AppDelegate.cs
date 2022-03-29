556b4801728190d11eb9f9f026d309b3571c2ded
Israel Soto <issoto@microsoft.com>
2022-03-29 01:26:07 -0600

    [Google/TagManager] Added sample to test .net6.0

---
+++ samples/Google/TagManager/TagManagerSample/AppDelegate.cs
@@ -0,0 +1,47 @@
+using Xamarin.iOS.Shared.Helpers;
+
+namespace TagManagerSample;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+		// You can download your GoogleService-Info.plist file following the next link:
+		// https://firebase.google.com/docs/ios/setup
+		if (!GoogleServiceInfoPlistHelper.FileExist ()) {
+			Window = GoogleServiceInfoPlistHelper.CreateWindowWithFileNotFoundMessage ();
+			return true;
+		}
+
+		var coreType = typeof (Firebase.Core.App);
+		var installationsType = typeof (Firebase.Installations.Installations);
+		var firebaseAnalyticsType = typeof (Firebase.Analytics.Analytics);
+		var googleAnalyticsType = typeof (Google.Analytics.Gai);
+		var tagManagerType = typeof (Google.TagManager.ICustomFunction);
+		Firebase.Core.App.Configure ();
+
+		// create a UIViewController with a single UILabel
+		var vc = new UIViewController ();
+		vc.View!.AddSubview (new UILabel (Window!.Frame) {
+			BackgroundColor = UIColor.White,
+			TextAlignment = UITextAlignment.Center,
+			Lines = 0,
+			Text = $"Hello, iOS!\nRunning {nameof (TagManagerSample)} with .net6.0!"
+		});
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		return true;
+	}
+}
+
