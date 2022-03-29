fb7889b359e97c0b5cbc7f69a441ebc093f35e4a
Israel Soto <issoto@microsoft.com>
2022-03-28 19:49:59 -0600

    [Google/MobileAds] Added sample to test .net6.0

---
+++ samples/Google/MobileAds/MobileAdsExample/AppDelegate.cs
@@ -0,0 +1,33 @@
+namespace MobileAdsExample;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+		var mobileAdsType = typeof (Google.MobileAds.MobileAds);
+
+		// create a UIViewController with a single UILabel
+		var vc = new UIViewController ();
+		vc.View!.AddSubview (new UILabel (Window!.Frame) {
+			BackgroundColor = UIColor.White,
+			TextAlignment = UITextAlignment.Center,
+			Lines = 0,
+			Text = $"Hello, iOS!\nRunning {nameof (MobileAdsExample)} with .net6.0!"
+		});
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		return true;
+	}
+}
+
