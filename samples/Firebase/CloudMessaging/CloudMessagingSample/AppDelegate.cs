ee3e7b7c91f892d4482c8d671c52b9e70461ab16
Israel Soto <issoto@microsoft.com>
2022-03-31 00:35:53 -0600

    [Samples] Changed text color labels to black

--- samples/Firebase/CloudMessaging/CloudMessagingSample/AppDelegate.cs
+++ samples/Firebase/CloudMessaging/CloudMessagingSample/AppDelegate.cs
@@ -30,6 +30,7 @@ public class AppDelegate : UIApplicationDelegate {
 		var vc = new UIViewController ();
 		vc.View!.AddSubview (new UILabel (Window!.Frame) {
 			BackgroundColor = UIColor.White,
+			TextColor = UIColor.Black,
 			TextAlignment = UITextAlignment.Center,
 			Lines = 0,
 			Text = $"Hello, iOS!\nRunning {nameof (CloudMessagingSample)} with .net6.0!"
--- samples/Firebase/RemoteConfig/RemoteConfigSample/AppDelegate.cs
+++ samples/Firebase/RemoteConfig/RemoteConfigSample/AppDelegate.cs
@@ -31,6 +31,7 @@ public class AppDelegate : UIApplicationDelegate {
 		var vc = new UIViewController ();
 		vc.View!.AddSubview (new UILabel (Window!.Frame) {
 			BackgroundColor = UIColor.White,
+			TextColor = UIColor.Black,
 			TextAlignment = UITextAlignment.Center,
 			Lines = 0,
 			Text = $"Hello, iOS!\nRunning {nameof (RemoteConfigSample)} with .net6.0!"
